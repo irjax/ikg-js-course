b2c9bd60cacfb003965096d235a9bc1e3a601a3c
irjax <irjax001@gmail.com>
2025-09-30 13:17:25 -0500

    ToDo List - Practice Task

--- mod3/todoList/todo_list.js
+++ mod3/todoList/todo_list.js
@@ -2,6 +2,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAlldBtn = document.getElementById("clearAllBtn");
 
 let tasks = [];
 
@@ -39,6 +40,12 @@ function clearCompletedTasks() {
   displayTasks();
 }
 
+function clearAllTasks() {
+  tasks = [];
+  displayTasks();
+}
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAllTasks);
 displayTasks();
